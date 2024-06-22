388945a1c9cbbf2aef193a7acae3150208180ea0
rourkescott27 <126876499+rourkescott27@users.noreply.github.com>
2024-06-22 14:18:40 +0200

    implemented-mongoose-methods

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,40 @@ import express from 'express';
 import mongoose from 'mongoose';
 
 import { PORT, mongoDBURL } from './config.js';
+import { Book } from './models/bookModel.js';
 
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (request, response) => {
     console.log(request);
     return response.status(234).send("Welcome to the Bookstore");
+});
+
+app.post('/books', async (request, response) => {
+    try {
+        if (!request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: 'Please provide all the required information'
+            });
+        }
+
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear
+        };
+        const book = await Book.create(newBook);
+        return response.status(201).send(book);
+
+    } catch (error) {
+        console.log(error);
+        response.status(500).send({ message: error.message });
+    }
 });
 
 mongoose
