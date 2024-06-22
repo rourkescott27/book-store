e04e0aa1298a9e6443d67ffdacb27f8eefa377cc
rourkescott27 <126876499+rourkescott27@users.noreply.github.com>
2024-06-22 13:26:23 +0200

    created-first-http-route

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,10 @@ import { PORT } from './config.js';
 
 const app = express();
 
+app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(234).send("Welcome to the Bookstore");
+});
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
 });
