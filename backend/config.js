02fe1442d246d98fc377a86a474c2519334d8b38
rourkescott27 <126876499+rourkescott27@users.noreply.github.com>
2024-06-22 13:52:12 +0200

    connected-to-mongoDB

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+
+export const mongoDBURL =
+    "mongodb+srv://admin:J8C8Ub1j73wE@bookstore-cluster.rzntb6u.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Bookstore-Cluster";
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
-import { PORT } from './config.js';
+import mongoose from 'mongoose';
+
+import { PORT, mongoDBURL } from './config.js';
 
 const app = express();
 
@@ -7,6 +9,15 @@ app.get('/', (request, response) => {
     console.log(request);
     return response.status(234).send("Welcome to the Bookstore");
 });
-app.listen(PORT, () => {
-    console.log(`Server is running on port: ${PORT}`);
-});
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('Connected to MongoDB');
+        app.listen(PORT, () => {
+            console.log(`Server is running on port: ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
