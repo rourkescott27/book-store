8506c615286bd525beddb17e22aba8dca78b4a2f
rourkescott27 <126876499+rourkescott27@users.noreply.github.com>
2024-06-27 16:12:21 +0200

    completed-modal-view-and-functionality

--- frontend/src/components/home/BooksCard.jsx
+++ frontend/src/components/home/BooksCard.jsx
@@ -1,10 +1,10 @@
-import BooksSingleCard from "./BooksSingleCard";
+import BookSingleCard from "./BookSingleCard";
 
 const BooksCard = ({ books }) => {
     return (
         <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
             {books.map((item) => (
-                <BooksSingleCard key={item._id} book={item} />
+                <BookSingleCard key={item._id} book={item} />
             ))}
         </div>
     );
